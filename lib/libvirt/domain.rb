cb1956114dff63b2463aa17013a22bc0f26f088a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-10-17 10:30:08 -0700

    Improved domain functionality and tests using the test driver

--- lib/libvirt/domain.rb
+++ lib/libvirt/domain.rb
@@ -17,6 +17,34 @@ module Libvirt
       domain_info[:state]
     end
 
+    # Returns the maximum memory (in KB) allowed on this domain.
+    #
+    # @return [Integer]
+    def max_memory
+      domain_info[:maxMem]
+    end
+
+    # Returns the memory (in KB) currently allocated to this domain.
+    #
+    # @return [Integer]
+    def memory
+      domain_info[:memory]
+    end
+
+    # Returns the number of virtual CPUs for this domain.
+    #
+    # @return [Integer]
+    def virtual_cpus
+      domain_info[:nrVirtCpu]
+    end
+
+    # Returns the CPU time used in nanoseconds.
+    #
+    # @return [Integer]
+    def cpu_time_used
+      domain_info[:cpuTime]
+    end
+
     protected
 
     # Queries the domain info from libvirt to get standard information
--- test/libvirt/domain_test.rb
+++ test/libvirt/domain_test.rb
@@ -3,7 +3,33 @@ require "test_helper"
 Protest.describe("domain") do
   setup do
     @klass = Libvirt::Domain
+
+    # TODO: Define our own domain so we have more control over what we're
+    # testing.
+    @instance = Libvirt::Connection.connect("test:///default").domains.first
   end
 
-  # TODO: Need to use test driver to create test VMs.
+  should "provide the state of the domain" do
+    assert_equal :running, @instance.state
+  end
+
+  should "provide the maximum memory available on the domain" do
+    assert_equal 8388608, @instance.max_memory
+  end
+
+  should "provide the memory allocated to the domain" do
+    assert_equal 2097152, @instance.memory
+  end
+
+  should "provide the number of virtual CPUs attached to the domain" do
+    assert_equal 2, @instance.virtual_cpus
+  end
+
+  should "provide the CPU time used in nanoseconds" do
+    # This result is constantly changing so all we can really test is that
+    # its properly returned
+    result = nil
+    assert_nothing_raised { result = @instance.cpu_time_used }
+    assert result.is_a?(Integer)
+  end
 end
