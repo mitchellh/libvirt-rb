ce6692b8c5fda6c5c2c293b24e0ad76885d65793
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-10-17 22:40:08 -0700

    Also scope virterror functions by libvirt version

--- lib/ffi/libvirt/error_functions.rb
+++ lib/ffi/libvirt/error_functions.rb
@@ -1,17 +1,21 @@
 module FFI
   module Libvirt
-    # Functions from virterror.h in the order they appear
-    attach_function :virGetLastError, [], :virErrorPtr
-    attach_function :virSaveLastError, [], :virErrorPtr
-    attach_function :virResetLastError, [], :void
-    attach_function :virResetError, [:virErrorPtr], :void
-    attach_function :virFreeError, [:virErrorPtr], :void
-    attach_function :virConnGetLastError, [:virConnectPtr], :virErrorPtr
-    attach_function :virConnResetLastError, [:virConnectPtr], :void
-    attach_function :virCopyLastError, [:virErrorPtr], :int
-    attach_function :virDefaultErrorFunc, [:virErrorPtr], :void
-    attach_function :virSetErrorFunc, [:void_pointer, :virErrorFunc], :void
-    attach_function :virConnSetErrorFunc, [:virConnectPtr, :void_pointer, :virErrorFunc], :void
-    attach_function :virConnCopyLastError, [:virConnectPtr, :virErrorPtr], :int
+    libvirt_version "0.6.0" do
+      attach_function :virGetLastError, [], :virErrorPtr
+      attach_function :virResetLastError, [], :void
+      attach_function :virResetError, [:virErrorPtr], :void
+      attach_function :virConnGetLastError, [:virConnectPtr], :virErrorPtr
+      attach_function :virConnResetLastError, [:virConnectPtr], :void
+      attach_function :virCopyLastError, [:virErrorPtr], :int
+      attach_function :virDefaultErrorFunc, [:virErrorPtr], :void
+      attach_function :virSetErrorFunc, [:void_pointer, :virErrorFunc], :void
+      attach_function :virConnSetErrorFunc, [:virConnectPtr, :void_pointer, :virErrorFunc], :void
+      attach_function :virConnCopyLastError, [:virConnectPtr, :virErrorPtr], :int
+    end
+
+    libvirt_version "0.6.1" do
+      attach_function :virSaveLastError, [], :virErrorPtr
+      attach_function :virFreeError, [:virErrorPtr], :void
+    end
   end
 end
