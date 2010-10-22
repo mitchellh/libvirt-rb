dcdda1e243d34c3257a2179f4c67ad2f3b984246
Sergio Rubio <sergio@rubio.name>
2010-10-22 09:51:02 +0200

    * Added some doc comments to the create_domain.rb example * code tested with remote Xen and KVM Hypervisors

--- examples/create_domain.rb
+++ examples/create_domain.rb
@@ -7,13 +7,33 @@ require "libvirt"
 # how it works currently.
 
 spec = Libvirt::Spec::Domain.new
+
+#
+# Example of valid hypervisors
+# :kvm
+# :xen
+# :test
+#
 spec.hypervisor = :test
 spec.name = "My Test VM"
+#
+# :hvm (FullVirt in KVM anx Xen)
+# :linux (Xen PV)
+#
 spec.os.type = :hvm
 spec.memory = 123456 # KB
 
 # Connect to the test hypervisor so we don't actually create a
 # domain somewhere.
+#
+# Example of valid URIs:
+# vbox:///system (local virtualbox)
+# qemu+tcp://10.0.0.1/system (remote qemu over TCP)
+# qemu+ssh://10.0.0.1/system (remote qemu over SSH)
+# qemu+unix:///system (local qemu over unix socket)
+# xen+tcp://10.0.0.1/ (remote Xen over TCP)
+#
+# If no URI is given, libvirt does its best to guess.
 conn = Libvirt::Connection.connect("test:///default")
 
 # This creates the domain on the hypervisor without starting it.
