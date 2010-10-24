990fe268d9a1c4b1469e08d7f78cd1020a7397b0
David Calavera <david.calavera@gmail.com>
2010-10-24 16:28:06 +0200

    remove trailing spaces

--- test/libvirt/spec/encryptation_test.rb
+++ test/libvirt/spec/encryptation_test.rb
@@ -11,7 +11,7 @@ Protest.describe('Spec::Encryptation') do
       :type => 'passphrase',
       :uuid => 'c1f11a6d-8c5d-4a3e-ac7a-4e171c5e0d4a'
     }).to_xml
-    
+
     assert_xpath 'passphrase', enc, '//encryptation/secret/@type'
     assert_xpath 'c1f11a6d-8c5d-4a3e-ac7a-4e171c5e0d4a', enc, '//encryptation/secret/@uuid'
   end
