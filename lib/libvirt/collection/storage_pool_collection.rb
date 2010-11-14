c739447f675a8bade6a7d97032e0d952e1a44df9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-11-14 16:59:24 -0600

    StoragePoolCollection searching by name and UUID

--- lib/libvirt/collection/storage_pool_collection.rb
+++ lib/libvirt/collection/storage_pool_collection.rb
@@ -2,12 +2,35 @@ module Libvirt
   module Collection
     # Represents a collection of storage pools.
     class StoragePoolCollection < AbstractCollection
+      # Search for a storage pool. This will first search by name and
+      # then by UUID, returning the first result returned.
+      #
+      # @return [StoragePool]
+      def find(value)
+        result = find_by_name(value) rescue nil
+        result ||= find_by_uuid(value) rescue nil
+      end
+
+      # Search for a storage pool by name.
+      #
+      # @return [StoragePool]
+      def find_by_name(name)
+        nil_or_object(FFI::Libvirt.virStoragePoolLookupByName(interface, name), StoragePool)
+      end
+
+      # Search for a storage pool by UUID.
+      #
+      # @return [StoragePool]
+      def find_by_uuid(uuid)
+        nil_or_object(FFI::Libvirt.virStoragePoolLookupByUUIDString(interface, uuid), StoragePool)
+      end
+
       # Returns the inactive storage pools.
       #
       # @return [Array<StoragePool>]
       def inactive
         read_array(:virConnectListDefinedStoragePools, :virConnectNumOfDefinedStoragePools, :string).collect do |name|
-          nil_or_object(FFI::Libvirt.virStoragePoolLookupByName(interface, name), StoragePool)
+          find_by_name(name)
         end
       end
 
@@ -16,7 +39,7 @@ module Libvirt
       # @return [Array<StoragePool>]
       def active
         read_array(:virConnectListStoragePools, :virConnectNumOfStoragePools, :string).collect do |name|
-          nil_or_object(FFI::Libvirt.virStoragePoolLookupByName(interface, name), StoragePool)
+          find_by_name(name)
         end
       end
 
--- test/libvirt/collection/storage_pool_collection_test.rb
+++ test/libvirt/collection/storage_pool_collection_test.rb
@@ -6,6 +6,23 @@ Protest.describe("storage pool collection") do
     @instance = @klass.new(Libvirt.connect("test:///default"))
   end
 
+  context "finding" do
+    should "be able to generally find" do
+      assert @instance.find("default-pool")
+      assert !@instance.find("foo")
+    end
+
+    should "be able to find by name" do
+      assert @instance.find_by_name("default-pool")
+      assert_raises(Libvirt::Exception::LibvirtError) { @instance.find_by_name("foo") }
+    end
+
+    should "be able to find by UUID" do
+      # TODO: Test happy-path
+      assert_raises(Libvirt::Exception::LibvirtError) { @instance.find_by_uuid("foo") }
+    end
+  end
+
   should "provide a list of active storage pools" do
     active = @instance.active
     assert active.is_a?(Array)
